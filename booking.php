90d19e672c21ab3315b3ef0c906a3dfac9093914
Zainab Alobed <zainabh647@gmail.com>
2024-11-23 15:06:56 +0300

    fixing css link

--- booking.php
+++ booking.php
@@ -13,7 +13,7 @@ if(!isset($_SESSION['user_id'] )){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Booking System</title>
     <!-- Bootstrap CSS -->
-    <link href="./booking.css" rel="stylesheet">
+    <link href="./css/booking.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     
     </head>
