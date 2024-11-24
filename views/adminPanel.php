89299bc9139c1e4387852d312217b1f1d4127c21
Mohamed Salah <mohamedsalahmlk@gmail.com>
2024-11-24 19:34:05 +0300

    link with css file and google,cloudflare fonts

--- views/adminPanel.php
+++ views/adminPanel.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Panel</title>
+    <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp" />
 
 </head>
 
