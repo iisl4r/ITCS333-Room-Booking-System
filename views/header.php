fc8531d78ad4d40add31be065d793653a2c53b0c
Mohamed Salah <mohamedsalahmlk@gmail.com>
2024-11-24 17:32:27 +0300

    Add header.php for inclusion in other pages

---
+++ views/header.php
@@ -0,0 +1,28 @@
+<header class="sticky">
+    <div class="logo">
+        <span class="material-icons">
+            <img src="/ITCS333-Room-Booking-System/img/logo.png" width="60" height="60" alt="Logo">
+        </span>
+    </div>
+    <nav class="nav-links">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Browse Rooms</a></li>
+            <li><a href="#">My Bookings</a></li>
+            <li><a href="#">Admin Panel</a></li>
+            <li class="ste"><a href="#">Sign in</a></li>
+        </ul>
+    </nav>
+    <div class="auth">
+        <a href="#" class="sign-in">Sign in
+            <span class="material-symbols-sharp">
+                arrow_right_alt
+            </span>
+        </a>
+    </div>
+    <div class="hamburger" id="hamburger">
+        <span class="material-symbols-sharp">
+            menu
+        </span>
+    </div>
+</header>
