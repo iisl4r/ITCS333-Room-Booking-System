ff170341d2a615da18b31e1cd887cf31fec9f7fd
Zozy <hamzaali200411@gmail.com>
2024-12-09 20:34:27 +0300

    Update DB_Creation.sql

--- sql/DB_Creation.sql
+++ sql/DB_Creation.sql
@@ -7,6 +7,10 @@ CREATE TABLE users (
     fname VARCHAR(20) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
+    major VARCHAR(100),
+    age INT,
+    phone_num VARCHAR(20),
+    profile_pic VARCHAR(255),
     role ENUM('admin', 'user') DEFAULT 'user',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
